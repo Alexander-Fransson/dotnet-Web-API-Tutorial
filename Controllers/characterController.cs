e417caead2eac06204f01414659c8825a50e6319
festusOp7 <70211809+festusOp7@users.noreply.github.com>
2022-04-11 21:55:58 +0200

    gör metoderna async

--- Controllers/characterController.cs
+++ Controllers/characterController.cs
@@ -3,6 +3,7 @@ using dotnet_Web_API_Tutorial.Models;
 using System.Collections.Generic;
 using System.Linq;
 using dotnet_Web_API_Tutorial.Services.CombatantService;
+using System.Threading.Tasks;
 
 namespace dotnet_Web_API_Tutorial.Controllers
 {
@@ -10,11 +11,6 @@ namespace dotnet_Web_API_Tutorial.Controllers
     [Route("[controller]")]
     public class characterController: ControllerBase
     {
-        private static List<Character> Combatants = new List<Character> {
-            new Character (),
-            new Character {ID = 1, Name = "Seagul"}
-        };
-
         private readonly ICombatantService _combatantService;
 
         public characterController(ICombatantService combatantService)
@@ -23,21 +19,20 @@ namespace dotnet_Web_API_Tutorial.Controllers
         }
 
         [HttpGet("GetAll")]
-        public ActionResult<Character> Get()
+        public async Task<ActionResult<Character>> Get()
         {
-            return Ok(Combatants);
+            return Ok(await _combatantService.GetAllCombatants());
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Character> GetOne(int id)
+        public async Task<ActionResult<Character>> GetOne(int id)
         {
-            return Ok(Combatants.FirstOrDefault(c => c.ID == id));
+            return Ok( await _combatantService.GetCombatanatByID(id));
         }
 
         [HttpPost]
-        public ActionResult<List<Character>> AddCombatant(Character newCombatant){
-            Combatants.Add(newCombatant);
-            return Ok(Combatants);
+        public async Task<ActionResult<List<Character>>> AddCombatant(Character newCombatant){
+            return Ok(await _combatantService.AddComatant(newCombatant));
         }
     }
 }
