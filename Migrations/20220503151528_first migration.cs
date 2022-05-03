3a67fc765d1ffdf455749ba7a922a852444401f2
festusOp7 <70211809+festusOp7@users.noreply.github.com>
2022-05-03 17:20:16 +0200

    migrerar och uppdaterar databas

---
+++ Migrations/20220503151528_first migration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dotnet_Web_API_Tutorial.Migrations
+{
+    public partial class firstmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Characters",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    HP = table.Column<int>(type: "int", nullable: false),
+                    SP = table.Column<int>(type: "int", nullable: false),
+                    DP = table.Column<int>(type: "int", nullable: false),
+                    IP = table.Column<int>(type: "int", nullable: false),
+                    Class = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Characters", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Characters");
+        }
+    }
+}
